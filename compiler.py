da71cb03ea56482b525b2ef11baa83880a0d9504
WencanHuang <huangwencan@fabu.ai>
2019-06-15 13:25:49 +0800

    add optimizer

--- compiler.py
+++ compiler.py
@@ -4,6 +4,7 @@ from MiniCParser import calcyacc
 from CodeGenerate import genmid
 from CodeGenerate import mid2masm32
 from SemanticAnalysis import scoped_symbol_table
+from Optimizer import optim
 from utils.visualize_syntax_tree import show_syntax_tree
 
 class Logger(object):
@@ -36,20 +37,18 @@ OPTIONS:
   -CC                     Include comments from within macros in preprocessed output
     ''')
 
-
+# help
 if len(sys.argv)==1:
     myhelp()
     exit()
 
-
+# analysis parse tree
 f = open(sys.argv[1])
 s = f.read()
 f.close()
-# print(s)
+
 result = calcyacc.parser.parse(s)
 
-# print(type(result), len(result))
-# print(result)
 if result==None:
     print('Error while generating abstract syntax tree.')
     exit()
@@ -57,27 +56,27 @@ if result==None:
 # show syntax tree by picture
 show_syntax_tree(result, 'a.syntax.gv', viewnow=True)
 
-
+# write syntax tree tuple to file
 f = open('a.syntax', 'w')
 f.write(str(result))
 f.close()
 
+# generate middle file
 sys.stdout = Logger('a.mid')
 genmid.parse_program(result)
 sys.stdout.log.close()
 sys.stdout = sys.stdout.terminal
 
-# print('******'*3)
-# print(genmid.symbol_table)
-
+# analysis symbol table and write to file
 f = open('a.symtab', 'w')
 f.write(scoped_symbol_table.show_symbol_table(genmid.symbol_table).strip())
 f.close()
 
+
+# generate target code and write it to file
 f = open('a.mid')
 prog = f.read().strip()
 f.close()
-
 prog=[_.strip() for _ in prog.split(">"*9)]
 
 asm_name=''
@@ -88,5 +87,15 @@ else:
 
 sys.stdout = Logger(asm_name)
 mid2masm32.parse_prog(prog)
+sys.stdout.log.close()
+sys.stdout = sys.stdout.terminal
+
+# do optimization and write it to file
+f=open(asm_name,"r")
+asm_prog=f.read().strip()
+f.close()
+
+sys.stdout = Logger(asm_name+'op')
+optim.optimize(asm_prog)
 sys.stdout.log.close()
 sys.stdout = sys.stdout.terminal
